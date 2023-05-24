651a79dbcb7ecb8062cbb4533e47fe182407c174
bevhatzl <beverleyhatzl@gmail.com>
2023-05-24 17:56:16 +1000

    fetch and transform the backend data

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,10 +1,20 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
 import { ExpensesContext } from '../store/expenses-context';
 import { getDateMinusDays } from '../util/date';
+import { fetchExpenses } from '../util/http';
 
 function RecentExpenses() {
   const expensesContext = useContext(ExpensesContext);
+
+  useEffect(() => {
+    async function getExpenses() {
+      // Add this function because you should not turn a useEffect into an async function
+      const expenses = await fetchExpenses();
+      expensesContext.setExpenses(expenses);
+    }
+    getExpenses();
+  }, []);
 
   const recentExpenses = expensesContext.expenses.filter((expense) => {
     const today = new Date();
--- store/expenses-context.js
+++ store/expenses-context.js
@@ -1,72 +1,10 @@
 import { createContext, useReducer } from 'react';
 
-const DUMMY_EXPENSES = [
-  {
-    id: 'e1',
-    description: 'A pair of shoes',
-    amount: 59.99,
-    date: new Date('2021-12-19'),
-  },
-  {
-    id: 'e2',
-    description: 'A pair of trousers',
-    amount: 89.29,
-    date: new Date('2022-10-05'),
-  },
-  {
-    id: 'e3',
-    description: '4 bananas',
-    amount: 5.99,
-    date: new Date('2023-04-20'),
-  },
-  {
-    id: 'e4',
-    description: 'a book',
-    amount: 49.99,
-    date: new Date('2023-04-15'),
-  },
-  {
-    id: 'e5',
-    description: 'another book',
-    amount: 18.59,
-    date: new Date('2023-04-22'),
-  },
-  {
-    id: 'e6',
-    description: 'A pair of shoes',
-    amount: 59.99,
-    date: new Date('2021-12-19'),
-  },
-  {
-    id: 'e7',
-    description: 'A pair of trousers',
-    amount: 89.29,
-    date: new Date('2022-10-05'),
-  },
-  {
-    id: 'e8',
-    description: '4 bananas',
-    amount: 5.99,
-    date: new Date('2023-04-20'),
-  },
-  {
-    id: 'e9',
-    description: 'a book',
-    amount: 49.99,
-    date: new Date('2023-05-05'),
-  },
-  {
-    id: 'e10',
-    description: 'another book',
-    amount: 18.59,
-    date: new Date('2023-05-06'),
-  },
-];
-
 // The initial value passed in here is not really used but will help with auto-complete where the context is used.
 export const ExpensesContext = createContext({
   expenses: [],
   addExpense: ({ description, amount, date }) => {},
+  setExpenses: (expenses) => {},
   deleteExpense: (id) => {},
   updateExpense: (id, { description, amount, date }) => {},
 });
@@ -76,6 +14,8 @@ function expensesReducer(state, action) {
     case 'ADD':
       const id = new Date().toString() + Math.random().toString();
       return [{ ...action.payload, id: id }, ...state];
+    case 'SET':
+      return action.payload;
     case 'UPDATE':
       const updateableExpenseIndex = state.findIndex(
         (expense) => expense.id === action.payload.id
@@ -93,10 +33,14 @@ function expensesReducer(state, action) {
 }
 
 function ExpensesContextProvider({ children }) {
-  const [expensesState, dispatch] = useReducer(expensesReducer, DUMMY_EXPENSES);
+  const [expensesState, dispatch] = useReducer(expensesReducer, []);
 
   function addExpense(expenseData) {
     dispatch({ type: 'ADD', payload: expenseData });
+  }
+
+  function setExpenses(expenses) {
+    dispatch({ type: 'SET', payload: expenses });
   }
 
   function deleteExpense(id) {
@@ -109,6 +53,7 @@ function ExpensesContextProvider({ children }) {
 
   const value = {
     expenses: expensesState,
+    setExpenses,
     addExpense,
     deleteExpense,
     updateExpense,
